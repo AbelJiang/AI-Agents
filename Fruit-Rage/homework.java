f67c9f53f725e33a53e938e80a4c464986cff5b8
Wen Jiang <abeljiang@hotmail.com>
2017-10-15 16:08:56 -0700

    Read Input File

--- Fruit-Rage/homework.java
+++ Fruit-Rage/homework.java
@@ -1,5 +1,27 @@
+import java.io.*;
+import java.util.*;
+
 public class homework{
-	public static void main(String[] args){
-	
+	static int N=0; //Board Size
+	static int P=0; //Fruit Types
+	static float T=0;
+	public static void main(String[] args) throws IOException{
+		
+
+		BufferedReader br=new BufferedReader(new FileReader("./input.txt"));
+		N=Integer.parseInt(br.readLine());
+		P=Integer.parseInt(br.readLine());
+		T=Float.parseFloat(br.readLine());
+		System.out.println(N);
+
+		char[][] InitState=new char[N][N];
+		String row=null;
+		for(int i=0;i<N;i++){
+			row=br.readLine();
+			for(int j=0;j<N;j++){
+				InitState[i][j]=row.charAt(j);
+			}
+		}
+		br.close();
 	}
 }
